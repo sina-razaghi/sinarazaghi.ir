1c1521d94f97008870ac72d5a1e17a0e9aaf4b9e
sina-razaghi <sina.razaghi79@gmail.com>
2024-03-28 15:09:35 +0330

    change primary color to blue

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-
 $(document).ready(function() {
   AOS.init( {
     // uncomment below for on-scroll animations to played only once
